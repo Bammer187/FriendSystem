197397c076773f09a52f7328212f4c4a8ad5fa1b
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-16 20:01:56 +0200

    ISSUE-001: Only execute command if commandSender is a player

--- src/main/java/me/yinzuro/friendSystem/commands/FriendAddCommandExecutor.java
+++ src/main/java/me/yinzuro/friendSystem/commands/FriendAddCommandExecutor.java
@@ -3,11 +3,15 @@ package me.yinzuro.friendSystem.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class FriendAddCommandExecutor implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
+        if (commandSender instanceof Player) {
+            Player player = (Player) commandSender;
+        }
         return false;
     }
 }
