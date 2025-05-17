98495062d39b8fdfbfbbe6d4a2d478a21faf5b63
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-17 21:53:31 +0200

    ISSUE-010: Checks if command sender is a player

--- src/main/java/me/yinzuro/friendSystem/commands/FriendDenyCommandExecutor.java
+++ src/main/java/me/yinzuro/friendSystem/commands/FriendDenyCommandExecutor.java
@@ -3,11 +3,16 @@ package me.yinzuro.friendSystem.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class FriendDenyCommandExecutor implements CommandExecutor {
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
+        if (!(commandSender instanceof Player player)) {
+            commandSender.sendMessage("§cYou aren't a player");
+            return true;
+        }
         return false;
     }
 }
