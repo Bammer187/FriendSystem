0e11b91b4f15308d0716596c0b6ccbcececc97c9
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-17 22:00:20 +0200

    ISSUE-010: Created try catch block

--- src/main/java/me/yinzuro/friendSystem/commands/FriendDenyCommandExecutor.java
+++ src/main/java/me/yinzuro/friendSystem/commands/FriendDenyCommandExecutor.java
@@ -1,15 +1,20 @@
 package me.yinzuro.friendSystem.commands;
 
+import me.yinzuro.friendSystem.FriendSystem;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.SQLException;
 
 public class FriendDenyCommandExecutor implements CommandExecutor {
+
+    private final JavaPlugin plugin = FriendSystem.getInstance();
+
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
         if (!(commandSender instanceof Player player)) {
@@ -29,6 +34,13 @@ public class FriendDenyCommandExecutor implements CommandExecutor {
         } else if (friend == player) {
             player.sendMessage("§cYou can't deny yourself.");
             return true;
+        }
+
+        try {
+            denyRequest(friend, player);
+        } catch (SQLException e) {
+            player.sendMessage("§cThere was an error while denying the request.");
+            plugin.getLogger().severe("MySQL-ERROR while trying to delete from open_friend_requests: " + e.getMessage());
         }
 
         return false;
