a4af2ea6bb97cef8b48506cbc0ff7d981e8d6cca
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-17 19:02:01 +0200

    ISSUE-009: Created friend accept command executor

--- src/main/java/me/yinzuro/friendSystem/commands/FriendCommandExecutor.java
+++ src/main/java/me/yinzuro/friendSystem/commands/FriendCommandExecutor.java
@@ -8,6 +8,7 @@ import org.jetbrains.annotations.NotNull;
 public class FriendCommandExecutor implements CommandExecutor {
 
     private final FriendAddCommandExecutor addExecutor = new FriendAddCommandExecutor();
+    private final FriendAcceptCommandExecutor acceptExecutor = new FriendAcceptCommandExecutor();
 
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
@@ -17,16 +18,20 @@ public class FriendCommandExecutor implements CommandExecutor {
         }
 
         String subcommand = strings[0].toLowerCase();
+        String[] subStrings = new String[strings.length - 1];
+        System.arraycopy(strings, 1, subStrings, 0, subStrings.length);
 
         switch (subcommand) {
-            case "add":
-                String[] subStrings = new String[strings.length - 1];
-                System.arraycopy(strings, 1, subStrings, 0, subStrings.length);
+            case "add": {
                 return addExecutor.onCommand(commandSender, command, s, subStrings);
-
-            default:
+            }
+            case "accept": {
+                return acceptExecutor.onCommand(commandSender, command, s, subStrings);
+            }
+            default: {
                 commandSender.sendMessage("§cUnknown command.");
                 return true;
+            }
         }
     }
 }
