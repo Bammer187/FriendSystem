59c92b3f9213b1ada58d4cb32d70630b05d4a526
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-17 11:44:21 +0200

    ISSUE-011: Created DataBaseManager constructor

--- src/main/java/me/yinzuro/friendSystem/database/DatabaseManager.java
+++ src/main/java/me/yinzuro/friendSystem/database/DatabaseManager.java
@@ -1,4 +1,14 @@
 package me.yinzuro.friendSystem.database;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.sql.Connection;
+
 public class DatabaseManager {
+    private final JavaPlugin plugin;
+    private Connection connection;
+
+    public DatabaseManager(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
 }
