86a6d2cda59afa5ea2d681862d789add8a688ed7
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-17 14:57:13 +0200

    ISSUE-011: Created DatabaseManager createTables() method

--- src/main/java/me/yinzuro/friendSystem/database/DatabaseManager.java
+++ src/main/java/me/yinzuro/friendSystem/database/DatabaseManager.java
@@ -4,6 +4,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseManager {
@@ -23,6 +24,7 @@ public class DatabaseManager {
 
         String url = "jdbc:mysql://" + host + ":" + port + "/" + database + "?useSSL=false&autoReconnect=true";
         this.connection = DriverManager.getConnection(url, username, password);
+        createTables();
     }
 
     public void disconnect() {
@@ -42,6 +44,42 @@ public class DatabaseManager {
             return connection != null && !connection.isClosed();
         } catch (SQLException e) {
             return false;
+        }
+    }
+
+    private void createTables() throws SQLException {
+        String[] statements = {
+                """
+                CREATE TABLE IF NOT EXISTS players (
+                    uuid CHAR(36) PRIMARY KEY
+                );
+                """,
+                """
+                CREATE TABLE IF NOT EXISTS friends (
+                    player_uuid CHAR(36) NOT NULL,
+                    friend_uuid CHAR(36) NOT NULL,
+                    PRIMARY KEY (player_uuid, friend_uuid),
+                    FOREIGN KEY (player_uuid) REFERENCES players(uuid),
+                    FOREIGN KEY (friend_uuid) REFERENCES players(uuid)
+                );
+                """,
+                """
+                CREATE table IF NOT EXISTS open_friend_requests (
+                    player_uuid CHAR(36) NOT NULL,
+                    from_player_uuid CHAR(36) NOT NULL,
+                    PRIMARY KEY (player_uuid, from_player_uuid),
+                    FOREIGN KEY (player_uuid) REFERENCES players(uuid),
+                    FOREIGN KEY (from_player_uuid) REFERENCES players(uuid)
+                );
+                """
+        };
+
+        for (String statement : statements){
+            try (PreparedStatement ps = connection.prepareStatement(statement)) {
+                ps.execute();
+            } catch (SQLException e) {
+                plugin.getLogger().severe("MySQL-ERROR while creating tables: " + e.getMessage());
+            }
         }
     }
 }
