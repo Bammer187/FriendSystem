22b83da33f1f3636c5e55f2c735c4fd0d684d282
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-17 21:58:13 +0200

    ISSUE-010: Created denyRequest() method

--- src/main/java/me/yinzuro/friendSystem/commands/FriendDenyCommandExecutor.java
+++ src/main/java/me/yinzuro/friendSystem/commands/FriendDenyCommandExecutor.java
@@ -7,6 +7,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.sql.SQLException;
+
 public class FriendDenyCommandExecutor implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
@@ -30,5 +32,9 @@ public class FriendDenyCommandExecutor implements CommandExecutor {
         }
 
         return false;
+    }
+
+    private void denyRequest(Player fromPlayer, Player toPlayer) throws SQLException {
+
     }
 }
