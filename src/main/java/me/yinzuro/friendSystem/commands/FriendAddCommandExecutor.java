7ed0681c4d5d4b0e8d82407388c99b3c28e9588c
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-17 15:30:12 +0200

    ISSUE-001: Created insertRequestIntoDatabase() method

--- src/main/java/me/yinzuro/friendSystem/commands/FriendAddCommandExecutor.java
+++ src/main/java/me/yinzuro/friendSystem/commands/FriendAddCommandExecutor.java
@@ -1,10 +1,12 @@
 package me.yinzuro.friendSystem.commands;
 
+import me.yinzuro.friendSystem.FriendSystem;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import net.kyori.adventure.text.Component;
@@ -12,9 +14,17 @@ import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.event.HoverEvent;
 import static net.kyori.adventure.text.Component.text;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.UUID;
+
 public class FriendAddCommandExecutor implements CommandExecutor {
+
+    private final JavaPlugin plugin = FriendSystem.getInstance();;
+
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
+
         if (!(commandSender instanceof Player player)) {
             commandSender.sendMessage("§cYou aren't a player");
             return true;
@@ -46,5 +56,21 @@ public class FriendAddCommandExecutor implements CommandExecutor {
         friend.sendMessage(message);
 
         return false;
+    }
+
+    private void insertRequestIntoDatabase (Player fromPlayer, Player toPlayer) throws SQLException {
+        try (PreparedStatement statement = FriendSystem.getDatabase().getConnection().prepareStatement("""
+                INSERT INTO open_friend_requests (player_uuid, friend_uuid) VALUES (?, ?)""")) {
+
+            UUID playerUUID = fromPlayer.getUniqueId();
+            UUID friendUUID = toPlayer.getUniqueId();
+
+            statement.setString(1, playerUUID.toString());
+            statement.setString(2, friendUUID.toString());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            fromPlayer.sendMessage("§cThere was an error while saving to database");
+            plugin.getLogger().severe("MySQL-ERROR while inserting into open_friend_requests" + e.getMessage());
+        }
     }
 }
