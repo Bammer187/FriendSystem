6599cf831bfa63e7439733e95f751e3311c7e661
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-18 16:01:09 +0200

    ISSUE-016: Moved saveToDatabaseLastMessage() to MessageUtils

--- src/main/java/me/yinzuro/friendSystem/utils/MessageUtils.java
+++ src/main/java/me/yinzuro/friendSystem/utils/MessageUtils.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 
 public class MessageUtils {
 
-    private static boolean checkIfPlayerAreFriends(Player player1, Player player2) throws SQLException {
+    private static boolean checkIfPlayersAreFriends(Player player1, Player player2) throws SQLException {
         String query = "SELECT 1 FROM friends WHERE player_uuid = ? AND friend_uuid = ? LIMIT 1;";
         try (Connection conn = FriendSystem.getDatabase().getConnection();
              PreparedStatement ps = conn.prepareStatement(query)) {
@@ -21,6 +21,22 @@ public class MessageUtils {
             try (ResultSet rs = ps.executeQuery()) {
                 return rs.next();
             }
+        }
+    }
+
+    private static void saveToDatabaseLastMessage(Player player1, Player player2) throws SQLException {
+        String query = "UPDATE last_message SET friend_uuid = ? WHERE player_uuid = ?;";
+
+        try (Connection conn = FriendSystem.getDatabase().getConnection();
+             PreparedStatement ps = conn.prepareStatement(query)) {
+
+            ps.setString(1, player1.getUniqueId().toString());
+            ps.setString(2, player2.getUniqueId().toString());
+            ps.executeUpdate();
+
+            ps.setString(1, player2.getUniqueId().toString());
+            ps.setString(2, player1.getUniqueId().toString());
+            ps.executeUpdate();
         }
     }
 }
