4400c4c68fe5054013d727c98b5aac64c9909366
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-18 13:10:21 +0200

    ISSUE-004: Created checkIfPlayerAreFriends() method

--- src/main/java/me/yinzuro/friendSystem/commands/MessageCommandExecutor.java
+++ src/main/java/me/yinzuro/friendSystem/commands/MessageCommandExecutor.java
@@ -7,6 +7,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.sql.SQLException;
+
 public class MessageCommandExecutor implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
@@ -29,6 +31,10 @@ public class MessageCommandExecutor implements CommandExecutor {
             return true;
         }
 
+        return false;
+    }
+
+    private boolean checkIfPlayerAreFriends(Player player1, Player player2) throws SQLException {
         return false;
     }
 }
