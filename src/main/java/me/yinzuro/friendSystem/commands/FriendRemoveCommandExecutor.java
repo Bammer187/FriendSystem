7db37960f8c5059f678ff477e4b9ef86648084c4
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-18 10:07:00 +0200

    ISSUE-003: Added check if the friend specified exists or is the commandSender

--- src/main/java/me/yinzuro/friendSystem/commands/FriendRemoveCommandExecutor.java
+++ src/main/java/me/yinzuro/friendSystem/commands/FriendRemoveCommandExecutor.java
@@ -1,5 +1,6 @@
 package me.yinzuro.friendSystem.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,6 +12,15 @@ public class FriendRemoveCommandExecutor implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
         if (!(commandSender instanceof Player player)) {
             commandSender.sendMessage("§cYou aren't a player.");
+            return true;
+        }
+
+        Player friend = Bukkit.getPlayerExact(strings[0]);
+        if (friend == null) {
+            player.sendMessage("§cCouldn't find a player with the given name.");
+            return true;
+        } else if (friend == player) {
+            player.sendMessage("§cYou don't have yourself as friend.");
             return true;
         }
 
