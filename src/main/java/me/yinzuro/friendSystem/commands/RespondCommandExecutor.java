680fcb33f1b49da08fe06be2c5ad26c44a63f89e
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-18 15:43:33 +0200

    ISSUE-005: Created getLastFriendMessagedUUID() method

--- src/main/java/me/yinzuro/friendSystem/commands/RespondCommandExecutor.java
+++ src/main/java/me/yinzuro/friendSystem/commands/RespondCommandExecutor.java
@@ -6,6 +6,9 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.sql.SQLException;
+import java.util.UUID;
+
 public class RespondCommandExecutor implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
@@ -20,5 +23,9 @@ public class RespondCommandExecutor implements CommandExecutor {
         }
 
         return false;
+    }
+
+    private UUID getLastFriendMessagedUUID(Player player) throws SQLException {
+
     }
 }
