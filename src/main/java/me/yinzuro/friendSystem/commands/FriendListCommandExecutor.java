91b3ba30d748296eb13d075a699857d4343d444c
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-18 21:00:21 +0200

    ISSUE-006: Created check if the player used the command correctly

--- src/main/java/me/yinzuro/friendSystem/commands/FriendListCommandExecutor.java
+++ src/main/java/me/yinzuro/friendSystem/commands/FriendListCommandExecutor.java
@@ -6,6 +6,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.UUID;
+
 public class FriendListCommandExecutor implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
@@ -14,6 +16,21 @@ public class FriendListCommandExecutor implements CommandExecutor {
             return true;
         }
 
-        return false;
+        UUID playerUUID = player.getUniqueId();
+        String friendListPage = "";
+
+        if (strings.length == 0) {
+            friendListPage = "1";
+        }
+        else if (strings.length == 1) {
+            friendListPage = strings[0];
+        }
+        else {
+            player.sendMessage("§cUsage /friend list <page>");
+            return true;
+        }
+        player.sendMessage("§7[§4Friends]§7] List of your friends: §3Page " + friendListPage);
+
+        return true;
     }
 }
