65a6bb0ca121c00457b1da074ef1b81f37789310
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-19 21:05:44 +0200

    ISSUE-018: Created FriendNameGroups constructor

--- src/main/java/me/yinzuro/friendSystem/utils/FriendNameGroups.java
+++ src/main/java/me/yinzuro/friendSystem/utils/FriendNameGroups.java
@@ -1,4 +1,15 @@
 package me.yinzuro.friendSystem.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FriendNameGroups {
+
+    private final List<String> onlineFriends;
+    private final List<String> offlineFriends;
+
+    public FriendNameGroups(List<String> onlineFriends, List<String> offlineFriends) {
+        this.onlineFriends = onlineFriends != null ? onlineFriends : new ArrayList<>();
+        this.offlineFriends = offlineFriends != null ? offlineFriends : new ArrayList<>();
+    }
 }
