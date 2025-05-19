1156ed1468fc8f6d56f9bd31558a1534e71ce5d0
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-19 19:44:00 +0200

    ISSUE-007: Copied getAllFriends() method from FriendListCommandExecutor

--- src/main/java/me/yinzuro/friendSystem/listener/FriendListClickListener.java
+++ src/main/java/me/yinzuro/friendSystem/listener/FriendListClickListener.java
@@ -1,5 +1,6 @@
 package me.yinzuro.friendSystem.listener;
 
+import me.yinzuro.friendSystem.FriendSystem;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -12,6 +13,14 @@ import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class FriendListClickListener implements Listener {
 
@@ -54,5 +63,31 @@ public class FriendListClickListener implements Listener {
         if (event.getView().title().equals(title)) {
             event.setCancelled(true);
         }
+    }
+
+    private List<UUID> getAllFriends(Player player) throws SQLException {
+        String query = """
+        SELECT friend_uuid FROM friends
+        WHERE player_uuid = ?;
+        """;
+
+        List<UUID> friendUUIDs = new ArrayList<>();
+
+        try (Connection connection = FriendSystem.getDatabase().getConnection();
+             PreparedStatement ps = connection.prepareStatement(query)) {
+
+            ps.setString(1, player.getUniqueId().toString());
+
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    String uuidStr = rs.getString("friend_uuid");
+                    if (uuidStr != null) {
+                        friendUUIDs.add(UUID.fromString(uuidStr));
+                    }
+                }
+            }
+        }
+
+        return friendUUIDs;
     }
 }
