29b21ab0ab42a4e351524b6c1319075944465933
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-19 18:46:16 +0200

    ISSUE-007: Created check if an item was clicked

--- src/main/java/me/yinzuro/friendSystem/listener/FriendListClickListener.java
+++ src/main/java/me/yinzuro/friendSystem/listener/FriendListClickListener.java
@@ -1,14 +1,19 @@
 package me.yinzuro.friendSystem.listener;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class FriendListClickListener implements Listener {
 
     @EventHandler
     public void onItemClick(InventoryClickEvent event) {
         if (!(event.getWhoClicked() instanceof Player player)) return;
+
+        ItemStack clickedItem = event.getCurrentItem();
+        if (clickedItem == null || clickedItem.getType() == Material.AIR) return;
     }
 }
