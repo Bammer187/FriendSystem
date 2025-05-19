714c328f4774d2a79a2f772fe1f20533a7f1d705
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-19 18:32:41 +0200

    ISSUE-007: Created FriendListClickListener Class

---
+++ src/main/java/me/yinzuro/friendSystem/listener/FriendListClickListener.java
@@ -0,0 +1,13 @@
+package me.yinzuro.friendSystem.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class FriendListClickListener implements Listener {
+
+    @EventHandler
+    public void onItemClick(InventoryClickEvent event) {
+
+    }
+}
