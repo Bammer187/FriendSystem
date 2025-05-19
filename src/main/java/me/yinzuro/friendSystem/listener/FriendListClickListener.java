21b50d53de932b6d6063d53db6d9d6904211fe8b
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-19 19:34:41 +0200

    ISSUE-007: Created onInventoryClick() and onInventoryDrag() methods

--- src/main/java/me/yinzuro/friendSystem/listener/FriendListClickListener.java
+++ src/main/java/me/yinzuro/friendSystem/listener/FriendListClickListener.java
@@ -7,6 +7,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -36,5 +38,13 @@ public class FriendListClickListener implements Listener {
 
             player.openInventory(friendsInventory);
         }
+    }
+
+    public void onInventoryClick(InventoryClickEvent event) {
+
+    }
+
+    public void onInventoryDrag(InventoryDragEvent event) {
+
     }
 }
