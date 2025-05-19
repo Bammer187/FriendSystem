dd19b73a47fff8a37e3b313ebf44f037afc35f4a
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-19 18:44:50 +0200

    ISSUE-007: Created check if the command sender is a player

--- src/main/java/me/yinzuro/friendSystem/listener/FriendListClickListener.java
+++ src/main/java/me/yinzuro/friendSystem/listener/FriendListClickListener.java
@@ -1,5 +1,6 @@
 package me.yinzuro.friendSystem.listener;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -8,6 +9,6 @@ public class FriendListClickListener implements Listener {
 
     @EventHandler
     public void onItemClick(InventoryClickEvent event) {
-
+        if (!(event.getWhoClicked() instanceof Player player)) return;
     }
 }
