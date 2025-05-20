0db4360ede460120fda933f359d051d63423f6a6
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-20 17:08:33 +0200

    ISSUE-014: Created list with subcommands

--- src/main/java/me/yinzuro/friendSystem/commands/FriendCommandTabCompleter.java
+++ src/main/java/me/yinzuro/friendSystem/commands/FriendCommandTabCompleter.java
@@ -6,9 +6,13 @@ import org.bukkit.command.TabCompleter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class FriendCommandTabCompleter implements TabCompleter {
+
+    private final List<String> SUB_COMMANDS = Arrays.asList("add", "remove", "accept", "deny", "list");
+
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
         return List.of();
