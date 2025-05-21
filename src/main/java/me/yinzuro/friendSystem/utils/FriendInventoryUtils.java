584a63a5466dd7a6462d804432df16419c25ba40
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-21 19:02:28 +0200

    ISSUE-023: Implemented runLater() logic

--- src/main/java/me/yinzuro/friendSystem/utils/FriendInventoryUtils.java
+++ src/main/java/me/yinzuro/friendSystem/utils/FriendInventoryUtils.java
@@ -1,8 +1,14 @@
 package me.yinzuro.friendSystem.utils;
 
+import me.yinzuro.friendSystem.FriendSystem;
 import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FriendInventoryUtils {
+
+    private final JavaPlugin plugin = FriendSystem.getInstance();
+
     static final Component FRIEND_LIST = Component.text("§bYour friends");
     static final Component FRIEND_REQUESTS = Component.text("§bYour friend requests");
     static final Component ACCEPT_DENY = Component.text("§bFriend request");
@@ -14,6 +20,6 @@ public final class FriendInventoryUtils {
     }
 
     private void runLater(Runnable task) {
-
+        Bukkit.getScheduler().runTaskLater(plugin, task, 2L);
     }
 }
