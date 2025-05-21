e59a66e6ae947b4dc2fd1d5e7f13f392cefec796
Lukas Bammer <lukas.bammer@bulme.at>
2025-05-21 18:52:18 +0200

    ISSUE-023: Created titles in FriendInventoyUtils

--- src/main/java/me/yinzuro/friendSystem/utils/FriendInventoryUtils.java
+++ src/main/java/me/yinzuro/friendSystem/utils/FriendInventoryUtils.java
@@ -1,4 +1,10 @@
 package me.yinzuro.friendSystem.utils;
 
+import net.kyori.adventure.text.Component;
+
 public final class FriendInventoryUtils {
+    static final Component FRIEND_LIST = Component.text("§bYour friends");
+    static final Component FRIEND_REQUESTS = Component.text("§bYour friend requests");
+    static final Component ACCEPT_DENY = Component.text("§bFriend request");
+    static final Component REMOVE = Component.text("§bRemove friend");
 }
